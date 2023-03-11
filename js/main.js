7b7139d460c4eeca367c99645357c7dafa3ec1fb
Raazk10 <rakioraag10@gmail.com>
2023-03-11 13:41:56 +0100

    code cleanup

--- js/main.js
+++
@@ -1,3 +0,0 @@
-import getEventData from "./module/getEventData.js";
-
-getEventData();
--- js/module/fetchApi.js
+++
@@ -1,8 +0,0 @@
-const API_KEY = "aLGf7LIEfrlaEtpVXyO5Oj9InyDNM8gN";
-export default async function fetchApi(query) {
-  const response = await fetch(
-    `https://app.ticketmaster.com/discovery/v2/suggest?apikey=${API_KEY}&locale=en-us&keyword=${query}`
-  );
-  const data = await response.json();
-  return data;
-}
